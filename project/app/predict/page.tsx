a9b9fb386d6b0514d56cfe3f9850ed40d4aeb211
Vishva <sapariyavishva23@gmail.com>
2026-02-09 13:32:02 +0530

    Update page.tsx
    final api done

--- project/app/predict/page.tsx
+++ project/app/predict/page.tsx
@@ -147,7 +147,7 @@ export default function Predict() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://my-projectml.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
